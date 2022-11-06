2c75ad7186961471aedc10bbd78e802fc9171062
Michael Lamparski <diagonaldevice@gmail.com>
2022-11-05 21:12:48 -0400

    make thread

--- discord-server/discord/message.js
+++ discord-server/discord/message.js
@@ -2,18 +2,18 @@ import 'dotenv/config';
 import { DiscordRequest } from "./utils.js";
 
 export async function makeDiscordThread(content) {
-    console.log("post");
-    const x = await DiscordRequest(`channels/${process.env.DISCORD_CHANNEL_ID}/messages`, {
+    const res = await DiscordRequest(`channels/${process.env.DISCORD_CHANNEL_ID}/messages`, {
         method: "POST",
         body: {
-            "content": "Hello, World!",
+            "content": "A rocket is launching at ???????",
             "tts": false,
-            // "embeds": [{
-            //     "title": "Hello, Embed!",
-            //     "description": "This is an embedded message."
-            // }],
         },
     });
-    console.log(x);
-    return x;
+    const message = await res.json();
+    DiscordRequest(`channels/${process.env.DISCORD_CHANNEL_ID}/messages/${message.id}/threads`, {
+        method: "POST",
+        body: {
+            name: "Rocket launch at ?????",
+        },
+    });
 }
